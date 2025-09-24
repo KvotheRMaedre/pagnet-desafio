d0ede5748b1b3b6444fa112d2a5ef4931d630a75
Ítalo H. <italodssouza.backup@gmail.com>
2025-09-24 10:31:28 -0300

    First Commit

---
+++ batchms/src/main/java/tech/kvothe/batchms/BatchmsApplication.java
@@ -0,0 +1,13 @@
+package tech.kvothe.batchms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchmsApplication.class, args);
+	}
+
+}
---
+++ batchms/src/test/java/tech/kvothe/batchms/BatchmsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.kvothe.batchms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
